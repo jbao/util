f00c181769e02f6cdaca57de345f78deefce477f
Jie Bao <jie.bao@gmail.com>
2012-09-19 12:34:27 +0200

    description

---
+++ R/mfpt.R
@@ -0,0 +1,24 @@
+#' Plot expression time series for the given genes. 
+#'
+#' This function plots the expression profiles of the given genes.
+#'
+#' @param ctrl list of time series data under the control condition, contains
+#' the field dat, cond, tpoint
+#' @param treatd list of time series data under the treatment condition, contains
+#' the field dat, cond, tpoint
+#' @param filename filename
+#' @export
+get.mfpt.sp <- function(mfpt.mat, idmap, net, source.entrez, target.entrez) {
+    registerDoMC()
+    mfpt.sp <- foreach(i=1:length(source.entrez), .combine='rbind') %:%
+        foreach(j=1:length(target.entrez), .combine='rbind') %dopar% {
+            ii <- idmap$vertex[which(source.entrez[i] == idmap$entrez)] + 1
+            jj <- idmap$vertex[which(target.entrez[j] == idmap$entrez)] + 1
+            source.idx <- match(source.entrez[i], V(net)$gene)
+            target.idx <- match(target.entrez[j], V(net)$gene)
+            sp <- shortest.paths(net, source.idx, target.idx, weights=NA) # BFS
+            data.frame(mfpt=mfpt.mat[ii,jj], sp=sp,
+                source=get(source.entrez[i], org.Hs.egSYMBOL),
+                target=idmap$official[which(target.entrez[j]==idmap$entrez)])
+    }
+}
